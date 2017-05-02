2bf7c2257655a79c8584c094ee70e8dab754943d
Marcin Brzeziński <cintrzyk@gmail.com>
2017-05-02 22:07:42 +0200

    select only needed plugins

---
+++ config/initializers/tinymce.rb
@@ -0,0 +1,6 @@
+Alchemy::Tinymce.init = {
+  toolbar1: 'bold italic underline',
+  toolbar2: 'pastetext charmap code | undo redo | alchemy_link unlink | alignleft aligncenter alignright alignjustify',
+  plugins: 'link image print preview',
+  language: 'en_GB'
+}
